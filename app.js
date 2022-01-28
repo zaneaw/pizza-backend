1b62ad1458cdb8ecd98b713ddccab4fe27e4f572
Zane Wilson <wilsonzanea@gmail.com>
2022-01-28 13:08:55 -0500

    express generator REST API

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+let dishRouter = require("./routes/dishRouter");
+let leaderRouter = require("./routes/leaderRouter");
+let promoRouter = require("./routes/promoRouter");
 
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/dishes", dishRouter);
+app.use("/leaders", leaderRouter);
+app.use("/promotions", promoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
