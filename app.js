6e212d1e3f8ddc11949af9b38e50f18743a1ff41
Zane Wilson <wilsonzanea@gmail.com>
2022-01-31 18:07:17 -0500

    Express REST API with Mongoose part 1

--- app.js
+++ app.js
@@ -1,47 +1,63 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
-let dishRouter = require("./routes/dishRouter");
-let leaderRouter = require("./routes/leaderRouter");
-let promoRouter = require("./routes/promoRouter");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const dishRouter = require("./routes/dishRouter");
+const leaderRouter = require("./routes/leaderRouter");
+const promoRouter = require("./routes/promoRouter");
+
+const mongoose = require("mongoose");
+
+const Dishes = require("./models/dishes");
+
+const url = "mongodb://localhost:27017/conFusion";
+const connect = mongoose.connect(url);
+
+connect.then(
+  (db) => {
+    console.log("Connected correctly to the server");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
 
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 app.use("/dishes", dishRouter);
 app.use("/leaders", leaderRouter);
 app.use("/promotions", promoRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
