fa54331efff2b98801215ba8c6402b14cd6569cc
Zane Wilson <wilsonzanea@gmail.com>
2022-02-08 11:52:57 -0500

    CORS

--- routes/cors.js
+++ routes/cors.js
@@ -8,14 +8,16 @@ const whitelist = ["http://localhost:3000", "https://localhost:3443"];
 const corsOptionsDelegate = (req, callback) => {
   let corsOptions;
   // indexOf returns -1 if not found
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+  if (whitelist.indexOf(req.header("Origin")) !== -1) {
+    // origin of request found in whitelist, move forward
     corsOptions = { origin: true };
-  }
-  else {
+  } else {
+    // origin of request not found, move forward with error
     corsOptions = { origin: false };
   }
   callback(null, corsOptions);
 };
-
+// allow all CORS
 exports.cors = cors();
+// CORS restricted
 exports.corsWithOptions = cors(corsOptionsDelegate);
