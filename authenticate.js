9a9844fd579be1dd4156c13e4273fdd2b4e77cc6
Zane Wilson <wilsonzanea@gmail.com>
2022-04-06 19:18:22 -0400

    email

--- authenticate.js
+++ authenticate.js
@@ -14,7 +14,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 exports.getToken = (user) => {
-  return jwt.sign(user, config.secretKey, { expiresIn: 360000 });
+    return jwt.sign(user, config.secretKey, { expiresIn: 360000 });
 };
 
 let opts = {};
@@ -22,59 +22,60 @@ opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.secretKey;
 
 exports.jwtPassport = passport.use(
-  new JwtStrategy(opts, (jwt_payload, done) => {
-    console.log("JWT payload: ", jwt_payload);
-    User.findOne({ _id: jwt_payload._id }, (err, user) => {
-      if (err) {
-        return done(err, false);
-      } else if (user) {
-        return done(null, user);
-      } else {
-        return done(null, false);
-      }
-    });
-  })
+    new JwtStrategy(opts, (jwt_payload, done) => {
+        console.log("JWT payload: ", jwt_payload);
+        User.findOne({ _id: jwt_payload._id }, (err, user) => {
+            if (err) {
+                return done(err, false);
+            } else if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        });
+    })
 );
 
 exports.verifyUser = passport.authenticate("jwt", { session: false });
 
 exports.verifyAdmin = (req, res, next) => {
-  console.log("Admin Request: ", req.user.admin);
-  if (req.user.admin) {
-    next();
-  } else {
-    res.statusCode = 403;
-    res.end("Only admins are allowed here.");
-    (err) => next(err);
-  }
+    console.log("Admin Request: ", req.user.admin);
+    if (req.user.admin) {
+        next();
+    } else {
+        res.statusCode = 403;
+        res.end("Only admins are allowed here.");
+        (err) => next(err);
+    }
 };
 
 exports.facebookPassport = passport.use(
-  new FacebookTokenStrategy(
-    {
-      clientID: config.facebook.clientId,
-      clientSecret: config.facebook.clientSecret,
-    },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ facebookId: profile.id }, (err, user) => {
-        if (err) {
-          return done(err, false);
-        } else if (!err && user !== null) {
-          return done(null, user);
-        } else {
-          user = new User({ username: profile.displayName });
-          user.facebookId = profile.id;
-          user.firstname = profile.name.givenName;
-          user.lastname = profile.name.familyName;
-          user.save((err, user) => {
-            if (err) {
-              return done(err, false);
-            } else {
-              return done(null, user);
-            }
-          });
+    new FacebookTokenStrategy(
+        {
+            clientID: config.facebook.clientId,
+            clientSecret: config.facebook.clientSecret,
+        },
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({ facebookId: profile.id }, (err, user) => {
+                if (err) {
+                    return done(err, false);
+                } else if (!err && user !== null) {
+                    return done(null, user);
+                } else {
+                    user = new User({ username: profile.displayName });
+                    user.facebookId = profile.id;
+                    user.firstname = profile.name.givenName;
+                    user.lastname = profile.name.familyName;
+                    user.email = profile.email;
+                    user.save((err, user) => {
+                        if (err) {
+                            return done(err, false);
+                        } else {
+                            return done(null, user);
+                        }
+                    });
+                }
+            });
         }
-      });
-    }
-  )
+    )
 );
