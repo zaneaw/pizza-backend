2a61d3ad4195ed72c65c8a7fbb8f0fc88129ac9f
Zane Wilson <wilsonzanea@gmail.com>
2022-04-07 11:52:35 -0400

    add timestamps

--- models/user.js
+++ models/user.js
@@ -2,25 +2,30 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
 
-const User = new Schema({
-    firstname: {
-        type: String,
-        default: "",
+const User = new Schema(
+    {
+        firstname: {
+            type: String,
+            default: "",
+        },
+        lastname: {
+            type: String,
+            default: "",
+        },
+        email: {
+            type: String,
+            default: "",
+        },
+        facebookId: String,
+        admin: {
+            type: Boolean,
+            default: false,
+        },
     },
-    lastname: {
-        type: String,
-        default: "",
-    },
-    email: {
-        type: String,
-        default: "",
-    },
-    facebookId: String,
-    admin: {
-        type: Boolean,
-        default: false,
-    },
-});
+    {
+        timestamps: true,
+    }
+);
 
 User.plugin(passportLocalMongoose);
 
